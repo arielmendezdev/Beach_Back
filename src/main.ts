df2e02067b3dc8f0adc00945a74d9f6cd85f746b
ariosmen <arielosmen@hotmail.com>
2025-01-18 18:57:08 -0300

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'production') {
+    app.useLogger(['error', 'warn']); // Solo registra errores y advertencias
+  } else {
+    app.useLogger(['log', 'debug', 'error', 'warn', 'verbose']);
+  }
+
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true
   }))
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 
