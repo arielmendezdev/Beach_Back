8e5685cb28e897748c1a846a1babe9e307771777
ariosmen <arielosmen@hotmail.com>
2025-01-18 10:00:17 -0300

    refactor

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
+
 import { Client } from 'database/models/clients.model';
-import { SequelizeModule } from '@nestjs/sequelize';
 import { Address } from 'database/models/address.model';
 import { Tent } from 'database/models/tent.model';
 import { Umbrella } from 'database/models/umbrella.model';
