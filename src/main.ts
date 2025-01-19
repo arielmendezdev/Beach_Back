5583581e5d1815e0057f44c18a017433f0c3b391
ariosmen <arielosmen@hotmail.com>
2025-01-19 10:38:34 -0300

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true
