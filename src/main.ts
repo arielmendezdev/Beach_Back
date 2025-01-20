89a3cb8b571c7be9b16b31a7f6ef0d915cf255bb
ariosmen <arielosmen@hotmail.com>
2025-01-20 18:55:46 -0300

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Server } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const server: Server = app.getHttpAdapter().getInstance();
 
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true
@@ -14,6 +16,8 @@ async function bootstrap() {
   // app.setGlobalPrefix('api/v1') // Esto es para asignarle a todas las urls el prefijo
 
   await app.listen(process.env.PORT ?? 3000);
+
+  return server;
 }
 
 export default bootstrap();
